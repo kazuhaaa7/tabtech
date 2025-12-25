dfa81372d15a1ae816230628352ef82daab27180
Muhammad Fathor Rossi <fthrrssi@gmail.com>
2025-12-25 19:13:32 +0700

    atur readme

--- main.py
+++ main.py
@@ -5,10 +5,34 @@ app = Flask(__name__) # bawaan flask
 # ===
 
 # karna flask ini pemograman berbasis web dan itu memrelukan page web. route yg akan menjadi penghubungnya
-@app.route('/') # -> bawaan flask | home page
+# home page
+@app.route('/') # -> bawaan flask 
+def home():
+    return '<h1> Hello page </h1>' 
 
-def hp():
-    return '<h1> Hello cantik </h1>' 
+# contact page
+@app.route('/contact')
+def contact():
+    return '<h1> Contact page </h1>'
+
+# about page
+@app.route('/about')
+def about():
+    return '<h1> Abouit page </h1>'
+
+# profile page
+@app.route('/profile', defaults={'_route': 'home', 'username': 'ahmad'})
+# def profil():
+#     return '<h1> Profile </h1>'
+
+# membuat user dengan input manual lewat url
+@app.route('/profile/<string:username>', defaults={'_route': 'profile'})
+def profil_name(username, _route):
+    if _route == "home":
+        return '<h1> iki home page  cak </h1>'
+    elif _route == "profile":
+        username = username + ", loh"
+        return '<h1> Hai %s iki profile page cak</h1>' % username
 
 app.run(debug=True)
 # adanya parameter <debug=True> bertujuan untuk menguodate ouput code secara otomatis tanpa pencet tombol running or ketik 'python .py', dan tinggal refresh di webnya.
