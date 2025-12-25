6398d3ad43ba6f4ed18bfd15c811712be3e058bf
Muhammad Fathor Rossi <fthrrssi@gmail.com>
2025-12-25 20:55:58 +0700

    atur struktur

---
+++ tabtech/Logic-database/db/database.py
@@ -0,0 +1,22 @@
+import psycopg2
+from dotenv import load_dotenv 
+import os
+
+def connect_db():
+    load_dotenv()
+    try :
+        # create connection
+        connection = psycopg2.connect(
+            host = os.getenv('DB_HOST'),
+            port = os.getenv('DB_PORT'),
+            database = os.getenv('DB_NAME'),
+            user =  os.getenv('DB_USER'),
+            password = os.getenv('DB_PW')
+        )
+        print("Berhasil terhubung ke PostgreSQL!")
+        return connection
+
+    except Exception as e:
+        print("Gagal terhubung PostgreSQL", e)
+        return None
+
